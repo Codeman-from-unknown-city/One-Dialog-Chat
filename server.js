84a64a2580097227fe4d728e2a25aafde38e44cd
Codeman-from-unknown-city <58555698+Codeman-from-unknown-city@users.noreply.github.com>
2020-06-18 01:02:18 +0900

    Git training

---
+++ server.js
@@ -0,0 +1,115 @@
+const fs = require('fs');
+const http = require('http');
+const WebSocket = require('ws');
+
+const routing = {
+    '/': fs.readFileSync('./static/index.html', 'utf-8'),
+    '/client.js': fs.readFileSync('./static/client.js', 'utf-8'),
+    '/style.css': fs.readFileSync('./static/style.css', 'utf-8'),
+    '/favicon.ico': fs.readFileSync('./static/favicon.ico'),
+};
+
+const setContentType = (res, url) => {
+    let ext = url.split('.')[1];   
+    console.log(ext)
+    if (ext === 'css' || ext === 'html') res.setHeader('Content-Type', `text/${ext}`);
+    if (ext === 'ico') res.setHeader('Content-Type', 'image/x-icon');
+    if (ext === 'js') res.setHeader('Content-Type', 'text/javascript');
+}
+
+const server = http.createServer(function(req, res) {
+    if (req.url === '/') res.setHeader('Content-Type', 'text/html');
+    else setContentType(res, req.url);
+    const file = routing[req.url];
+    res.writeHead('200');
+    res.end(file);
+});
+
+server.listen(8000);
+
+const ws = new WebSocket.Server({
+    server,
+    clientTracking: true,
+});
+
+ws.on('connection', chatLogic);
+
+const users = [];
+
+function getUserIndex(arr, socket) {
+    let userIndex;
+    arr.forEach((user, index) => {if (user.socket === socket) userIndex = index});
+    return userIndex;
+}
+
+function sendUpdateUsersList() {
+    users.forEach(user => {
+        const usersCopy = users.slice();
+        usersCopy.splice(getUserIndex(usersCopy, user.socket), 1);
+        const withoutSockets = usersCopy.map(user => ({name: user.name, id: user.id}));
+        user.socket.send(JSON.stringify({type: 'usersList', names: withoutSockets}));
+    });
+}
+
+function chatLogic(socket) {
+    let collocutor;
+    socket.on('message', jsonMessage => {
+        const data = JSON.parse(jsonMessage);
+        console.log(data);
+        switch(data.type) {
+            case 'userName':
+                users.push({name: data.name, socket, id: Date.now()});
+                sendUpdateUsersList();
+                break;
+
+            case 'sendInvite':
+                let senderId;
+                let recipientSocket;
+                users.forEach(user => {
+                    if (user.socket === socket) senderId = user.id;
+                    if (user.id === +data.id) recipientSocket = user.socket;
+                });
+                recipientSocket.send(JSON.stringify({type: 'invite', fromName: data.from, fromId: senderId}));
+                break;
+
+            case 'responseToInvitation':
+                users.forEach(user => {
+                    if (user.id === data.toId) {
+                        if (data.response === 'ok') {
+                            user.socket.send(JSON.stringify({
+                                type: 'responseToInvitation',
+                                response: 'ok', 
+                                fromName: data.receiverName, 
+                                id: users[getUserIndex(users, socket)].id,
+                            }));
+                            collocutor = user;
+                        } else user.socket.send(JSON.stringify({
+                            type: 'responseToInvitation',
+                            response: data.response, 
+                            fromName: data.receiverName,
+                        })); 
+                    }
+                });
+                break;
+
+            case 'begin':
+                users.forEach(user => {
+                    if (user.id === data.id) {
+                        collocutor = user;
+                        collocutor.socket.send(JSON.stringify({type: 'invite', name: data.name, beginChat: true}));
+                    }
+                });
+                break;
+                
+            case 'message':
+                collocutor.socket.send(JSON.stringify({type: 'message', message: data.message,}));
+                break;
+        }
+    });
+
+    socket.on('close', () => {
+        if (collocutor) collocutor.socket.send(JSON.stringify({type: 'out'}));
+        users.splice(getUserIndex(users, socket), 1);
+        sendUpdateUsersList();
+    });
+}
---
+++ static/client.js
@@ -0,0 +1,179 @@
+const APP = document.getElementById('app');
+
+function debounse(fn, ms) {
+    let isCooldown = false;
+
+    return function(...args) {
+        if (isCooldown) return;
+        isCooldown = true;
+        setTimeout(() => isCooldown = false, ms);
+        
+        fn(...args);
+    }
+}
+
+function createNode(tag, className, inside, ...events) {
+    const node = document.createElement(tag);
+
+    if (className) node.className = className;
+
+    if (inside) node.innerHTML = inside;
+
+    for (let i = 0; i < events.length; i += 2) {
+        const eventName = events[i];
+        const eventFn = events[i + 1];
+        node.addEventListener(eventName, eventFn);
+    }
+    
+    return node;
+}
+
+const multiAppend = (parent, ...childs) => childs.forEach(child => parent.append(child));
+
+(function login(){
+    const loginWrap = createNode('div', 'login-wrap');
+    const appTitle = createNode('h1', 'app-title', 'One-Dialog-Chat');
+    const form = createNode('form', 'login');
+    const title = createNode('h2', 'login-title', 'Введите имя:');    
+    const input = createNode('input', 'login-input');    
+    const button = createNode('button', 'login-button', 'ok', 'click', chat);
+    multiAppend(form, title, input, button);
+    multiAppend(loginWrap, appTitle, form);
+    APP.append(loginWrap);
+})();
+
+function chat(event, name) {
+    event.preventDefault();
+
+    const userName = name ? name : document.querySelector('.login-input').value;
+    APP.innerHTML = '';
+    const invites = createNode('div', 'invites');
+    invites.insertAdjacentHTML('afterbegin', '<h1>Приглашения:</h1>');
+    const usersListWrap = createNode('div', 'list-wrap');
+    const anwsers = createNode('div', 'anwsers');
+    anwsers.insertAdjacentHTML('afterbegin', '<h1>Ответы:</h1>');
+    const usersList = createNode('ul', 'users-list');
+    usersListWrap.append(usersList);
+    multiAppend(APP, invites, usersListWrap, anwsers);
+
+    function addUser(user) {
+        const sendInvite = debounse((event) => {
+            ws.send(JSON.stringify({type: 'sendInvite', id: user.id, from: userName}));
+            event.target.innerHTML = 'Отправлено!';
+            setTimeout(() => event.target.innerHTML = 'Пригласить в чат', 30000);
+        }, 30000); 
+        const onlineUser = createNode('li', 'user');
+        const name = createNode('span', 'user-name', user.name);
+        const inviteBtn = createNode('button', 'invite-btn', 'Пригласить в чат', 'click', sendInvite);
+        multiAppend(onlineUser, name, inviteBtn);
+        usersList.append(onlineUser);
+    }
+
+    function showOnlineUsers(onlineUsers) {
+        usersList.innerHTML = '';
+        if (onlineUsers.length) {
+            usersList.insertAdjacentHTML('afterbegin', '<h1>Пользователи онлайн:</h1>');
+            onlineUsers.forEach(addUser);
+        } else usersList.insertAdjacentHTML('afterbegin', '<h1>Нет пользователей онлайн</h1>');
+    }
+
+    function showNotification(sentence, isInvite, fromWhomId) {
+        const notification = createNode('div', 'notification');
+        notification.insertAdjacentHTML('afterbegin', '<h3>Новое уведомление!</h3>');
+        const info = createNode('p', 'info', sentence);
+    
+        const hideNotification = () => notification.remove();
+    
+        if (isInvite) {
+            const options = createNode('div', 'options');
+            const sayOk = () => ws.send(JSON.stringify({ type: 'responseToInvitation', response: 'ok', toId: fromWhomId, receiverName: userName }));
+            const sayNo = () => ws.send(JSON.stringify({ type: 'responseToInvitation', response: 'no', toId: fromWhomId, receiverName: userName }));
+            const okButton = createNode('button', 'ok-button', 'Согласиться', 'click', sayOk, 'click', hideNotification);
+            const noButton = createNode('button', 'no-button', 'Отказаться', 'click', sayNo, 'click', hideNotification);
+            multiAppend(options, okButton, noButton);
+            multiAppend(notification, info, options);
+            invites.append(notification);
+        }
+        else {
+            const removeButton = createNode('span', 'remove-button', '&#9746;', 'click', hideNotification);
+            multiAppend(notification, removeButton, info);
+            anwsers.append(notification);
+        }
+    }
+
+    const ws = new WebSocket('ws://localhost:8000/');
+
+    ws.onopen = () => ws.send(JSON.stringify({type: 'userName', name: userName}));
+
+    ws.onmessage = message => {
+        const data = JSON.parse(message.data);
+        console.log(data)
+        
+        switch(data.type) {
+            case 'usersList': 
+                showOnlineUsers(data.names);
+                break;
+            
+            case 'invite':
+                if (data.beginChat) {
+                    dialogInterface(ws, data.name, userName);
+                    break;
+                }
+                showNotification(`Пользователь ${data.fromName} приглашает вас в чат.`, true, data.fromId);
+                break;
+
+            case  'responseToInvitation':
+                if (data.response === 'ok') {
+                    dialogInterface(ws, data.fromName, userName);
+                    ws.send(JSON.stringify({type: 'begin', name: userName, id: data.id}));
+                } else if (data.response === 'no') showNotification(`Пользователь ${data.fromName} отклонил ваше приглашение в чат.`);
+                else showNotification(`Пользователь ${data.fromName} уже находится в переписке с другим человеком.`);
+                break;
+        }
+    }
+}
+
+function dialogInterface(ws, name, thisUserName) {
+    APP.innerHTML = '';
+    const nameInterlocutor = name;
+
+    const dialogWrap = createNode('div', 'dialog-wrap');
+
+    const leaveDialog = event => (ws.close(), chat(event, thisUserName));
+    const turnBack = createNode('div', 'back', null, 'click', leaveDialog);
+    turnBack.append(createNode('span', null, '&larr; Вернуться назад'));
+
+    const messages = createNode('div', 'messages');
+    const nameToSend = createNode('span', 'name-interlocutor', nameInterlocutor);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if (input.value !== '') {
+            ws.send(JSON.stringify({ type: 'message', message: input.value }));
+            messages.append(createNode('span', 'message', `Вы: ${input.value}`));
+            input.value = '';
+        }
+    };
+
+    const formWrap = createNode('div', 'form-wrap');
+    const form = createNode('form', 'send-form');
+    const input = createNode('input', 'send-input');
+    const sendButton = createNode('i', 'fas fa-paper-plane', null, 'click', sendMessage);
+    multiAppend(formWrap, form, input, sendButton);
+
+    multiAppend(dialogWrap, nameToSend, messages, formWrap);
+    multiAppend(APP, turnBack, dialogWrap)
+
+    ws.onmessage = message => {
+        console.log(message.data);
+        const data = JSON.parse(message.data);
+        if (data.type === 'message') messages.append(createNode('span', 'message', `${nameInterlocutor}: ${data.message}`));
+        if (data.type === 'out') {
+            messages.innerHTML = '';
+            messages.append(createNode('span', 'message', `Ваш собеседник ${nameInterlocutor} вышел из диалога.`));
+            input.disabled = true;
+        }
+        if (data.type === 'invite') ws.send(JSON.stringify({ type: 'responseToInvitation', response: 'inChat', toId: data.fromId, receiverName: thisUserName }));
+        if (data.type === 'responseToInvitation') ws.send(JSON.stringify({ type: 'responseToInvitation', response: 'inChat', toId: data.id, receiverName: thisUserName}));
+    };
+}
