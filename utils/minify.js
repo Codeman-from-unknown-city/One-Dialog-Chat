1d387274541d6d84f09147d074a9623fcfaf7c85
Codeman-from-unknown-city <58555698+Codeman-from-unknown-city@users.noreply.github.com>
2020-07-05 15:56:17 +0900

    getting rid of duplicate code

--- utils/minify.js
+++ utils/minify.js
@@ -1,12 +1,18 @@
 module.exports = (file, ext) => {
+    let minifyFile;
+
     switch(ext) {
         case 'css':
-            return file.replace(/(\W)\s+(\D)/g, '$1$2').replace(/(\W)\s+(.)/g, '$1$2').replace(/(\w)\s+(\W)/g, '$1$2').trim();
+            minifyFile = file.replace(/(\W)\s+(\D)/g, '$1$2').replace(/(\W)\s+(.)/g, '$1$2').replace(/(\w)\s+(\W)/g, '$1$2');
+            break;
 
         case 'html':
-            return file.replace(/\s+/g, ' ').replace(/> </g, '><').trim();
+            minifyFile = file.replace(/\s+/g, ' ').replace(/> </g, '><');
+            break;
 
         default:
             return file;
     }
+    
+    return minifyFile.trim();
 }
